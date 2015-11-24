f716c1b1d96cb66e9603941b74185369cfd7eed8
Aimee <aimeebethmj@gmail.com>
2015-11-24 12:50:09 +0000

    added slick to footer

--- footer.php
+++ footer.php
@@ -1,17 +1,41 @@
 		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<?php theHTML5BoilerplateDirectory(); ?>js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+		<script src="<?php theJQueryDirectory(); ?>jquery.min.js"></script>
         <script src="<?php theHTML5BoilerplateDirectory(); ?>js/plugins.js"></script>
         <script src="<?php theActiveThemeDirectory(); ?>js/main.js"></script>
 
+        <?php if (is_front_page()) : ?>
+
+            <!-- load Slick and apply it to .slider -->
+            <script type="text/javascript" src="<?php theSlickDirectory() ; ?>slick.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function()
+                {
+                    var slickOptions = 
+                    {
+                        dots: true,
+                        arrows: false,
+                        autoplay: true,
+                        autoplaySpeed: 8000, 
+                        speed: 300, 
+                        fade: true,
+                        infinite: true  
+                    }
+
+                    $('.slider').slick(slickOptions);
+                });
+            </script>
+
+        <?php endif; ?>
+
+
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
+        <!-- <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
             e.src='https://www.google-analytics.com/analytics.js';
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+        </script> -->
     </body>
 </html>
