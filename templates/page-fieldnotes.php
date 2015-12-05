80438aa3fa49b5b452d81704b8191ec5fb664564
Aimee <aimeebethmj@gmail.com>
2015-12-05 15:46:12 +0000

    field notes page - field notes overview page (not even bare bones yet)

---
+++ templates/page-fieldnotes.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Text Page
+*/
+?>
+
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+
+
+
+
+
+
+
+		<?php endwhile; ?>
+
+	<?php else : ?>
+
+		<?php get_template_part('templates/404'); ?>
+
+	<?php endif; ?>
+
+<?php get_footer(); ?>
