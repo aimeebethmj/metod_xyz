4e4a62779d24b6d254d6f47a3308259804b3ee72
Matteo Menapace <hello@baddeo.com>
2016-08-05 14:16:19 +0100

    Fieldnotes started [tbc]

--- templates/page-fieldnotes.php
+++ templates/page-fieldnotes.php
@@ -10,11 +10,34 @@ Template Name: Fieldnotes Page
 
     <?php while (have_posts()) : the_post(); ?>
 
+      <div class="container fieldnotes">        
+        <h2 class="main-heading"><?php echo get_the_title(); ?></h2> 
+        <ul>
+        <?php
+          // get all the pages from the 'fieldnote' category
+          $notes = get_posts( array( 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'menu_order', 'category_name' => 'fieldnote', 'posts_per_page' => 200 ) );
 
+          foreach($notes as $note) // for each note within notes
+          {     
+            $URL = get_page_link($note->ID);
+            $title = $note->post_title;
+            $image = wp_get_attachment_image_src( get_post_thumbnail_id($note->ID), 'large' );
+            $blurb = get_field('mini_blurb', $note->ID);
 
+            // print_r($image);
 
+            echo '<li class="one-half column">
+                    <img src="' . $image[0] . '">
+                    <a href="'. $URL .'">
+                      <h3>'. $title . '</h3>
+                    </a>
+                    <p>' . $blurb . '</p>
+                  </li>';
+          }
 
-
+        ?>  
+        </ul>
+      </div>
 
     <?php endwhile; ?>
 
