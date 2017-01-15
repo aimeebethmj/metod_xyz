55ed0d89521eec61e8738630fe5c2f214933b7d7
Matteo Menapace <hello@baddeo.com>
2017-01-15 13:48:05 +0000

    No scale no more

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 		<meta name="description" content="<?php bloginfo('description'); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1">
 
 		<?php // wp_head(); // wordpress head functions ?>
 
