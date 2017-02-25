a9055d56380485f0b2a9ccd99c5db4f82bd0bb77
Matteo Menapace <hello@baddeo.com>
2017-02-25 17:50:27 +0000

    Proper categories

--- templates/page-project.php
+++ templates/page-project.php
@@ -12,86 +12,80 @@ Template Name: Project Page
 
 
 		<!-- CAROUSEL -->
-		    <?php if( have_rows('carousel') ): ?>
+			<?php if( have_rows('carousel') ): ?>
 
-		    	<div class="container slider">
-				<?php while( have_rows('carousel') ): the_row(); 
+				<div class="container slider">
+				<?php while( have_rows('carousel') ): the_row();
 					// vars
-					$image             = get_sub_field('image');
-			    	$headline          = get_sub_field('headline');
-					$link              = get_sub_field('carousel_link');
+					$image = get_sub_field('image');
+					$headline = get_sub_field('headline');
+					$link = get_sub_field('carousel_link');
 				?>
 
 					<div class="row">
 						<img class="u-max-full-width" src="<?php echo $image['url']; ?>">
 						<h2 class="low-centered"><?php echo $headline; ?></h2>
 					</div>
-					
+
 				<?php endwhile; ?>
 				</div>
 			<?php endif; ?>
 
 		<!-- PROJECT DETAIL -->
-			<?php 
+			<?php
 				// vars
 				$projectBlurb = get_field('project_blurb');
 				$projectDate = get_field('project_date');
 			?>
 
-			<div class="container project">				
-				
-				
+			<div class="container project">
 				<div class="text-content">
 					<h1 class="sub-heading"><?php echo get_the_title(); ?></h1>
 					<h3 class="sub-sub-heading"><?php echo $projectBlurb;?></h3>
 					<h3 class="sub-sub-heading"><?php echo $projectDate;?></h3>
 					<div class="twelve columns"><?php the_content();?></div>
 				</div>
-					
 			</div>
 
-
 		<!-- PRACTICE FEED -->
-			<?php 
-				// vars
+			<?php
 				$heading = get_field('second_heading');
 			?>
 
-			<div class="container work">				
+			<div class="container work">
 				<h2 class="main-heading"><?php echo $heading; ?></h2>
 				<ul class="gallery">
-				
-					<?php
-						// get all the pages from 'main' category
-						$work_pages = get_posts( array( 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'menu_order', 'category_name' => 'art', 'posts_per_page' => 200 ) );
 
-						foreach($work_pages as $work_page) // for each work page within work pages
+					<?php
+						// get all the pages in the same category as this
+						$categories = get_the_category();
+						$category = $categories[0]->name;
+						$project_pages = get_posts(
+							array(
+								'post_type' => 'page',
+								'order' => 'ASC',
+								'orderby' => 'menu_order',
+								'category_name' => $category,
+								'posts_per_page' => 200,
+								'exclude' => array($post->ID)
+								)
+							);
+
+						foreach($project_pages as $project_page)
 						{
-							
-							$work_URL = get_page_link($work_page->ID);
-							$work_name = $work_page->post_title;
-							$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id($work_page->ID), 'large' );
+
+							$work_URL = get_page_link($project_page->ID);
+							$work_name = $project_page->post_title;
+							$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id($project_page->ID), 'large' );
 
 							// consoleLog($featured_image);
-							
+
 							echo '<li style="background-image:url(' . $featured_image[0] .');"><a href="' . $work_URL . '"><h2 class="centered">' . $work_name . '</h2></a></li>';
-						}	 
+						}
 					?>
-					
-				</ul>		
+
+				</ul>
 			</div>
-
-
-
-
-
-
-
-
-
-
-
-
 
 		<?php endwhile; ?>
 
